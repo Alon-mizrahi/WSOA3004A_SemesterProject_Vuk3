17e4f68e2688605ea9b67fc7d32341b3edb95f0c
Alon-mizrahi <alonmiz1@gmail.com>
2021-10-06 17:44:32 +0200

    added basic note playing UI.

--- Semester Project/Assets/Alon/Player/SongDetection.cs
+++ Semester Project/Assets/Alon/Player/SongDetection.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SongDetection : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class SongDetection : MonoBehaviour
 
     public float MaxIdleTime = 5f;
     public float IdleTimer;
+    public GameObject NotesUI;
 
     GetInteractables GetInteractablesScript;
 
@@ -21,6 +23,7 @@ public class SongDetection : MonoBehaviour
         SongBook = GameObject.Find("SongBook").GetComponent<SongBook>();
         IdleTimer = MaxIdleTime;
         GetInteractablesScript = gameObject.GetComponent<GetInteractables>();
+        NotesUI.SetActive(false);
     }
 
     // Update is called once per frame
@@ -35,10 +38,12 @@ public class SongDetection : MonoBehaviour
         if (IdleTimer > 0 && CurrentSong != "")
         {
             IdleTimer -= Time.deltaTime;
+
         }
         else if(IdleTimer <= 0)
         {
             ClearSong();
+            NotesUI.SetActive(false);
         }
 
         
@@ -49,33 +54,55 @@ public class SongDetection : MonoBehaviour
         IdleTimer = MaxIdleTime;
         CurrentSong = "";
         Debug.Log("song cleared");
+
+        for (int i = 1; i <= 6; i++) 
+        {
+            NotesUI.transform.Find("Note"+i).GetComponent<Image>().color = Color.white;
+        }
+
     }
 
     public void GetNote(string Key) 
     {
+        Image Note = NotesUI.transform.Find("Note1").GetComponent<Image>();
+        if (CurrentSong.Length == 1-1) { Note = NotesUI.transform.Find("Note1").GetComponent<Image>(); }
+        else if (CurrentSong.Length == 2-1) { Note = NotesUI.transform.Find("Note2").GetComponent<Image>(); }
+        else if (CurrentSong.Length == 3-1) { Note = NotesUI.transform.Find("Note3").GetComponent<Image>(); }
+        else if (CurrentSong.Length == 4-1) { Note = NotesUI.transform.Find("Note4").GetComponent<Image>(); }
+        else if (CurrentSong.Length == 5-1) { Note = NotesUI.transform.Find("Note5").GetComponent<Image>(); }
+        else if (CurrentSong.Length == 6-1) { Note = NotesUI.transform.Find("Note6").GetComponent<Image>(); }
+
+
+
+
+        NotesUI.SetActive(true);
         if (Key == "Up") 
         {
             CurrentSong += "1";//whatever note this will be
             IdleTimer = MaxIdleTime; //only if input detected
             Debug.Log("Song: " + CurrentSong);
+            Note.color = Color.red;
         }
         else if (Key == "Down")
         {
             CurrentSong += "2";//whatever note this will be
             IdleTimer = MaxIdleTime; //only if input detected
             Debug.Log("Song: " + CurrentSong);
+            Note.color = Color.blue;
         }
         else if (Key == "Left")
         {
             CurrentSong += "3";//whatever note this will be
             IdleTimer = MaxIdleTime; //only if input detected
             Debug.Log("Song: " + CurrentSong);
+            Note.color = Color.green;
         }
         else if (Key == "Right")
         {
             CurrentSong += "4";//whatever note this will be
             IdleTimer = MaxIdleTime; //only if input detected
             Debug.Log("Song: " + CurrentSong);
+            Note.color = Color.yellow;
         }
 
 
