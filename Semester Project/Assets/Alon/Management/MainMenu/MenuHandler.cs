b75bf07f70148ccce98d01065eb45ab31fbb2f4c
Alon-mizrahi <alonmiz1@gmail.com>
2021-11-13 10:40:18 +0200

    menu things

--- Semester Project/Assets/Alon/Management/MainMenu/MenuHandler.cs
+++ Semester Project/Assets/Alon/Management/MainMenu/MenuHandler.cs
@@ -17,25 +17,23 @@ public class MenuHandler : MonoBehaviour
     public Button SelectedBtn;
 
     int index = 0;
-
     int Length = 0;
 
+    //sliders
     public Slider SelectedSlider;
-    public int MusicVolStart;
-    public int SfxVolStart;
+    public Slider[] Sliders = new Slider[5];
+    public int MusicVol = 70;
+    public int SfxVol = 80;
+    int SIndex = 0;
+    int SLength = 0;
 
-    //GameObject myEventSystem;
-
-
-
-
-    //Controls PlayerContSys;
 
     // Start is called before the first frame update
     void Start()
     {
         //myEventSystem = GameObject.Find("EventSystem");
         ControllerHandler();
+        //InitializeVolume();
 
     }
 
@@ -45,22 +43,24 @@ public class MenuHandler : MonoBehaviour
 
     }
 
-    public void ControllerHandler() 
+    public void ControllerHandler()
     {
         //Debug.Log(InputScheme.currentControlScheme);
         //Debug.Log("HANDLER");
         Length = 0;
         index = 0;
-
+        SIndex = 0;
+        SLength = 0;
         //clean buttons
         for (int x = 0; x < Buttons.Length; x++) { Buttons[x] = null; }
+        for (int x = 0; x < Sliders.Length; x++) { Sliders[x] = null; }
 
 
-        if (Screen1.activeSelf) 
+        if (Screen1.activeSelf)
         {
-            for (int j = 0; j < Screen1.transform.childCount; j++) 
+            for (int j = 0; j < Screen1.transform.childCount; j++)
             {
-                if (Screen1.transform.GetChild(j).GetComponent<Button>() != null) 
+                if (Screen1.transform.GetChild(j).GetComponent<Button>() != null)
                 {
                     Buttons[index] = Screen1.transform.GetChild(j).GetComponent<Button>();
                     index++;
@@ -78,6 +78,12 @@ public class MenuHandler : MonoBehaviour
                     index++;
                     Length++;
                 }
+                if (Screen2.transform.GetChild(j).GetComponent<Slider>() != null)
+                {
+                    Sliders[SIndex] = Screen2.transform.GetChild(j).GetComponent<Slider>();
+                    SIndex++;
+                    SLength++;
+                }
             }
         }
         else if (Screen3.activeSelf)
@@ -94,62 +100,71 @@ public class MenuHandler : MonoBehaviour
         }
 
         index = 0;
+        SIndex = 0;
         SelectedBtn = Buttons[index];
-
+        SelectedSlider = Sliders[SIndex];
         if (InputScheme.currentControlScheme == "Gamepad") { ControllerUIChanger(); }
-        
+
     }
 
-    public void ControllerNextBtn(InputAction.CallbackContext NxtContext) 
+    public void ControllerNextBtn(InputAction.CallbackContext NxtContext)
     {
         //myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
-        if (NxtContext.started) 
+        if (NxtContext.started)
         {
-            if (index < Length-1) { SelectedBtn = Buttons[index + 1]; index++; ControllerUIChanger(); }
+            if (SIndex < SLength - 1) { SelectedSlider = Sliders[SIndex + 1]; SIndex++; }
+            else if (index < Length - 1) { SelectedBtn = Buttons[index + 1]; index++; ControllerUIChanger(); }
         }
     }
 
     public void ControllerPrevBtn(InputAction.CallbackContext PrevContext)
     {
         //myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
-        if (PrevContext.started) 
-        { 
+        if (PrevContext.started)
+        {
             if (index > 0) { SelectedBtn = Buttons[index - 1]; index--; ControllerUIChanger(); }
         }
     }
 
     public void ControllerSelectBtn(InputAction.CallbackContext SelectContext)
     {
-        if (SelectContext.started) 
+        if (SelectContext.started)
         {
             SelectedBtn.onClick.Invoke();
         }
     }
 
-    void ControllerUIChanger() 
+    void ControllerUIChanger()
     {
-        for (int i = 0; i < Buttons.Length; i++) 
+        for (int i = 0; i < Buttons.Length; i++)
         {
-            if (Buttons[i] != null) { Buttons[i].gameObject.GetComponent<Outline>().enabled = false; }        
+            if (Buttons[i] != null) { Buttons[i].gameObject.GetComponent<Outline>().enabled = false; }
         }
         SelectedBtn.gameObject.GetComponent<Outline>().enabled = true;
     }
 
 
-    public void Play() 
+    public void Play()
     {
         SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
 
-    public void Quite() 
-    { 
-        Application.Quit(); 
+    public void Quite()
+    {
+        Application.Quit();
     }
 
 
-    void InitializeVolume() 
+
+    public void SFXValChange()
     {
         
     }
+
+    public void MusicValChange()
+    {
+        //MusicVol = Mu
+    }
+
 
 }
