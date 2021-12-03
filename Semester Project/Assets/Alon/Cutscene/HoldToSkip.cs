0e4bcdc4e028744eedd0c9a99efb709dbf79fbae
Alon-mizrahi <alonmiz1@gmail.com>
2021-12-03 11:47:31 +0200

    update cutscene

--- Semester Project/Assets/Alon/Cutscene/HoldToSkip.cs
+++ Semester Project/Assets/Alon/Cutscene/HoldToSkip.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HoldToSkip : MonoBehaviour
 {
-    public float Timer;
+    public float MaxTime =3f;
+    float timeLeft;
+
     public bool Holding = false;
+
+    public Image FadeOut;
+
+    public Image TimerBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeLeft = MaxTime;
     }
 
     // Update is called once per frame
@@ -19,8 +27,25 @@ public class HoldToSkip : MonoBehaviour
     {
         if (Holding)
         {
-            Timer = Time.time;
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                TimerBar.fillAmount = timeLeft / MaxTime;
+            }
+            else 
+            {
+                StartCoroutine("OutroFade");
+            }
+
+
+            //MaxTime = Time.time;
+
+
         }
+        else { timeLeft = MaxTime; TimerBar.fillAmount = 0;  }
+
+        //if(Timer)
+
 
     }
 
@@ -39,6 +64,23 @@ public class HoldToSkip : MonoBehaviour
         }
 
 
+    }
+
+
+
+    IEnumerator OutroFade()
+    {
+        Holding = false;
+        float Fade = 0f;
+        for (int i = 0; i <= 200; i++)
+        {
+            yield return new WaitForSeconds(0.005f);
+            FadeOut.color = new Color(FadeOut.color.r, FadeOut.color.g, FadeOut.color.b, Fade);
+
+            Fade += 0.005f;
+        }
+
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
     }
 
 
